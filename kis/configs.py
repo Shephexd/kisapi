c3d3e7b53c6efb4299912e5aaa72d4cae3992f49
shephexd <shephexd@gmail.com>
2022-10-29 22:49:02 +0900

    BUGFIX apikey uppercase

--- kis/configs.py
+++ kis/configs.py
@@ -1,6 +1,6 @@
 import os
 
 
-apihost = os.getenv("KIS_APIHOST", "")
-appkey = os.getenv("KIS_APPKEY", "")
-appsecret = os.getenv("KIS_APPSECRET", "")
+APIHOST = os.getenv("KIS_APIHOST", "")
+APPKEY = os.getenv("KIS_APPKEY", "")
+APPSECRET = os.getenv("KIS_APPSECRET", "")
